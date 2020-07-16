6e1d49413dbb8e0fb4e6b997c2728e4a2387c3a4
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-07-16 15:37:10 +0200

    Add files via upload

---
+++ data/MaSocit_4.js
@@ -0,0 +1,8 @@
+var json_MaSocit_4 = {
+"type": "FeatureCollection",
+"name": "MaSocit_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Secteur": "Ma Société" }, "geometry": { "type": "Point", "coordinates": [ -0.689012593033616, 44.782343789055147 ] } }
+]
+}
